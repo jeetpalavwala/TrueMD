ef3c3537fcf5081507c0be54c15c287cd0692e87
Aayush <jainaayush05@gmail.com>
2013-09-01 15:31:01 +0530

    IC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oaayush::Application.config.secret_token = 'a3982a68307ed6c400bccd656c282af5343321d3863aebd6c2a3600a9b7eec91ce37bd09c785634cde83a4aa0dcd24c72e189ea156b024e3d4113c8e7a901843'
